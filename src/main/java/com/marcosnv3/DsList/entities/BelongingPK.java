4853f5e62afe6b4b7e76e9e6d654b593eeaa2faf
Marcos santos <marcosvn3@academico.ufs.br>
2024-11-13 21:58:25 -0300

    atualizando entidade

--- src/main/java/com/marcosnv3/DsList/entities/BelongingPK.java
+++ src/main/java/com/marcosnv3/DsList/entities/BelongingPK.java
@@ -15,7 +15,7 @@ public class BelongingPK {
     private Game game;
 
     @ManyToOne
-    @JoinColumn(name = "gameList_id")
+    @JoinColumn(name = "list_id")
     private GameList gameList;
 
     public BelongingPK() {}
